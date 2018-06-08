5a160c4a9775506bac38104a42389401fad16925
oleksandr-aleksandrov <oleksandr.aleksandrov22@gmail.com>
2018-06-08 03:08:20 +0300

    deleted test footer_enqueue_scripts

--- wp-content/themes/mb-2/functions.php
+++ wp-content/themes/mb-2/functions.php
@@ -215,15 +215,3 @@ function sj_remove_type_attr($tag)
     return preg_replace("/type=['\"]text\/(javascript|css)['\"]/", '', $tag);
 }
 
-
-function footer_enqueue_scripts()
-{
-    remove_action('wp_head', 'wp_print_scripts');
-    remove_action('wp_head', 'wp_print_head_scripts', 9);
-    remove_action('wp_head', 'wp_enqueue_scripts', 1);
-    add_action('wp_footer', 'wp_print_scripts', 5);
-    add_action('wp_footer', 'wp_enqueue_scripts', 5);
-    add_action('wp_footer', 'wp_print_head_scripts', 5);
-}
-
-add_action('after_setup_theme', 'footer_enqueue_scripts');

852de205f41e2abfdd8da812c81c3a73a68cd2c7
oleksandr-aleksandrov <oleksandr.aleksandrov22@gmail.com>
2018-06-13 11:05:03 +0300

    changed photo-item

--- wp-content/themes/mb-2/template-parts/1_4_album_part.php
+++ wp-content/themes/mb-2/template-parts/1_4_album_part.php
@@ -1,11 +1,11 @@
 <?php if (isset($post->ID)): ?>
     <div class="row position-relative photo-album-item align-items-center mb-5">
-        <div class="col-md-6 p-0 position-relative">
+        <div class="col-sm-6 col-md-6 p-0 position-relative photo-album-item__title">
             <h2><?php echo $post->post_title; ?></h2>
             <a class="position-cover photo-album-item_link" href="<?php echo get_post_permalink($post); ?>"></a>
         </div>
-        <div class="col-md-6 p-0">
-            <a class="d-block image-link" href="<?php the_permalink(); ?>">
+        <div class="col-sm-6 col-md-6 p-0 photo-album-item__image">
+            <a class="d-block" href="<?php the_permalink(); ?>">
                 <picture>
                     <source srcset="<?php the_post_thumbnail_url('archive_photo_thumbnails'); ?>"
                             media="(max-width: 580px)">
